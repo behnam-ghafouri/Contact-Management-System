3299200597993de150081360b4b19f4d0a645d28
behnam-ghafouri <55107911+behnam-ghafouri@users.noreply.github.com>
2019-09-23 23:12:35 -0400

    Update contacts.h

--- contacts.h
+++ contacts.h
@@ -1,34 +1,9 @@
-// ----------------------------------------------------------------------------
-// DO NOT remove the below 2 lines.
-// The below two lines is needed to ensure only one copy
-// of this header file is included when compiling the project.
-// (it is referred to as "safeguarding")
-// You will learn about this in C++ (OOP244/BTP200)
-// For now, simply leave this code in!
+
 #ifndef CONTACTS_H_
 #define CONTACTS_H_
-// ----------------------------------------------------------------------------
-
-/* -------------------------------------------
-Name:behnam	ghafouri
-Student number:
-Email:
-Section:
-Date:
-----------------------------------------------
-Assignment: 2
-Milestone:  4
----------------------------------------------- */
 
 
-//------------------------------------------------------
-// Structure Types
-//------------------------------------------------------
 
-// +-------------------------------------------------+
-// | NOTE:  Copy/Paste your Assignment-2 Milestone-3 |
-// |        structures below:                        |
-// +-------------------------------------------------+
 
 // Structure type Name declaration
 struct Name {
